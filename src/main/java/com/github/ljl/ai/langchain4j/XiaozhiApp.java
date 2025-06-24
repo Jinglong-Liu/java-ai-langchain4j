2dd6c71d63db146213eb72f0f9ca46dda79e49ae
Jinglong-Liu <1601029287@qq.com>
2025-06-24 22:20:45 +0800

    初始化项目

---
+++ src/main/java/com/github/ljl/ai/langchain4j/XiaozhiApp.java
@@ -0,0 +1,18 @@
+package com.github.ljl.ai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: java-ai-langchain4j
+ * @description:
+ * @author: ljl
+ * @create: 2025-06-24 22:17
+ **/
+
+@SpringBootApplication
+public class XiaozhiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhiApp.class, args);
+    }
+}
