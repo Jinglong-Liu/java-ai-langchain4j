6192925c404297539f23f21c48a89d9482bf5626
Jinglong-Liu <1601029287@qq.com>
2025-06-25 00:01:26 +0800

    feat: Qianwen, ai service

---
+++ src/main/java/com/github/ljl/ai/langchain4j/assistant/Assistant.java
@@ -0,0 +1,12 @@
+package com.github.ljl.ai.langchain4j.assistant;
+
+/**
+ * @program: java-ai-langchain4j
+ * @description:
+ * @author: ljl
+ * @create: 2025-06-24 23:35
+ **/
+
+public interface Assistant {
+    String chat(String userMessage);
+}
---
+++ src/test/java/com/github/ljl/ai/langchain4j/AIServiceTest.java
@@ -0,0 +1,30 @@
+package com.github.ljl.ai.langchain4j;
+
+import com.github.ljl.ai.langchain4j.assistant.Assistant;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @program: java-ai-langchain4j
+ * @description:
+ * @author: ljl
+ * @create: 2025-06-24 23:36
+ **/
+
+@SpringBootTest
+public class AIServiceTest {
+
+    @Resource
+    private OpenAiChatModel openAiChatModel;
+    @Test
+    public void testChat() {
+        //创建AIService
+        Assistant assistant = AiServices.create(Assistant.class, openAiChatModel);
+        //调用service的接口
+        String answer = assistant.chat("Hello");
+        System.out.println(answer);
+    }
+}
